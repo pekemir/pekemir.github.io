099bee3c0b93213321ac0b3fcb101f400c265a7e
pekemir <91409576+pekemir@users.noreply.github.com>
2021-10-06 17:23:09 +0200

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,32 +1,26 @@
-
-var contador=1
-
-const menu=elemet.findbyid
-
-var contadorr=1;
+$(document).ready(main);
+var contador=1;
 function main(){
-alert("holaassgggggs") ;
-
+    $('.BarraMbotonmenu').click(function(){
 		// $('nav').toggle(); toggle=aparece brusco
-alert(contador);
+console.log("click");
 console.log(contador);
 console.log('.Navmenu'.left);
 		if(contador == 1){
-alert("666");
-			document.getElementById ('.Navmenu').animate({
+			$('.Navmenu').animate({
               
 				right: '0'
 			});
 			contador = 0;
 		} else {
 			contador = 1;
-	document.getElementById	('.Navmenu').animate({
+			$('.Navmenu').animate({
                 
 				right: '-100%'
 			});
 		}
 
-	
+	});
 
 };
 function ira(pagina){
