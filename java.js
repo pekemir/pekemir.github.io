cbb3c9f2e32bd3985fe88643ca3b741bc72d3519
pekemir <91409576+pekemir@users.noreply.github.com>
2022-10-30 06:29:01 +0100

    aa
    aa

--- java.js
+++ java.js
@@ -5,9 +5,13 @@ const fondomenu = document.getElementById('header-fondomenu');
 const listamenu = document.getElementById('header-listamenu');
 const iconosredes = document.getElementById('footer-iconosredes');
 const insta = document.getElementById('index-insta');
+const portada=document.getElementById('portada')
 var ancho=1020;
 var primera=1;
-
+function cerrar(id) {
+  id.style.display='none'
+  
+}
 function revealheader() {
   
 
@@ -217,3 +221,132 @@ $('#opcion11').click(function () {
  
 
 })
+
+//COOKIES//
+const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+   const botonAceptarCookies2=document.getElementById('btn-aceptar-cookies2');
+   const botonAceptarCookies3=document.getElementById('btn-permitirselec-cookies');
+   
+   const botonconfigurarCookies = document.getElementById('btn-configurar-cookies');
+   const avisoCookies = document.getElementById('aviso-cookies');
+   const avisoCookies2 = document.getElementById('contenedor-seleccion-cookies');
+   const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
+   
+   const tablero1=document.getElementById("tablero-cookie-consentimiento")
+   const tablero2=document.getElementById("tablero-cookie-consentimiento-listado")
+   const tablero3=document.getElementById("tablero-cookie-consentimiento-solotexto")
+   
+   const switchh=document.getElementById("switchid");
+   const switchh1=document.getElementById("switchid1");
+   dataLayer = [];
+   function mostrarcookies(){
+	   scrollX=0;
+	avisoCookies.classList.add('activo');
+	fondoAvisoCookies.classList.add('activo');
+	if (localStorage.getItem('no-mostrar')) {
+		switchh.checked=true;	}
+	else{switchh.checked=false;};
+	if (localStorage.getItem('cookies-aceptadas')) {
+		switchh1.checked=true;	}
+	else{switchh1.checked=false;}
+	localStorage.removeItem('cookies-aceptadas');
+	localStorage.removeItem('no-mostrar');
+	
+
+
+	
+   }
+   if(!localStorage.getItem('no-mostrar')){
+	   avisoCookies.classList.add('activo');
+	   fondoAvisoCookies.classList.add('activo');
+	   switchh.checked=false;
+	   switchh1.checked=false;
+	   console.log("noaceptadas");
+   } else if(localStorage.getItem('cookies-aceptadas')){
+	   
+	   dataLayer.push({'event': 'cookies-aceptadas'});
+	   console.log("aceptadas");
+   }
+   
+   botonconfigurarCookies.addEventListener('click', () => {
+	   avisoCookies.classList.remove('activo');
+	   avisoCookies2.classList.add('activo');
+	   
+	   
+   });
+   botonAceptarCookies.addEventListener('click', () => {
+	   avisoCookies.classList.remove('activo');
+     console.log("ACEPTADAS");
+	   fondoAvisoCookies.classList.remove('activo');
+      avisoCookies2.classList.remove('activo');
+	   localStorage.setItem('cookies-aceptadas', true);
+	   localStorage.setItem('no-mostrar',true);
+	   dataLayer.push({'event': 'cookies-aceptadas'});
+   });
+   botonAceptarCookies2.addEventListener('click', () => {
+	   avisoCookies.classList.remove('activo');
+	   fondoAvisoCookies.classList.remove('activo');
+  		 avisoCookies2.classList.remove('activo');
+	   localStorage.setItem('cookies-aceptadas', true);
+	   localStorage.setItem('no-mostrar',true);
+	   dataLayer.push({'event': 'cookies-aceptadas'});
+   });
+   
+   botonAceptarCookies3.addEventListener('click', () => {
+	   avisoCookies.classList.remove('activo');
+	   fondoAvisoCookies.classList.remove('activo');
+   avisoCookies2.classList.remove('activo');
+   
+   if(switchh.checked){
+	   
+	   localStorage.setItem('no-mostrar',true);}
+	   else{
+	   localStorage.removeItem('no-mostrar');};
+   
+   if(switchh1.checked){
+	   
+	   localStorage.setItem('cookies-aceptadas', true);
+   dataLayer.push({'event': 'cookies-aceptadas'});}
+   else{
+	localStorage.removeItem('cookies-aceptadas');
+   };
+	   
+	   
+	   
+   });
+   
+   function seleccion(id){
+	   var scrollElements = document.getElementsByClassName("itemclic");
+	   console.log(scrollElements);
+	   scrollElements[0].classList.remove("itemclic")
+	   var bot=document.getElementById(id);
+	   bot.classList.add("itemclic");
+	   switch (id) {
+		   case "seleccion1":
+			   tablero2.style.display="none";
+			   tablero3.style.display="none";
+			   tablero1.style.display="block";
+			   break;
+			   case "seleccion2":
+				   tablero2.style.display="block";
+			   tablero1.style.display="none";
+			   tablero3.style.display="none";
+			   break;
+			   case "seleccion3":
+				   tablero3.style.display="block";
+				   tablero1.style.display="none";
+				   tablero2.style.display="none";
+				   break;
+		   
+	   
+		   default:
+			   break;
+	   }
+	   /*que mostramos*/
+   }
+   function box(bot){
+	   console.log("box");
+	   console.log(bot);
+	   var bot2=document.getElementById(bot);
+	   console.log(bot2.checked);
+   }
